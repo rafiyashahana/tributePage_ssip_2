3321e40e60b6189199c0d7d45d4afc246f28af0c
shahana <rafiyashahana@gmail.com>
2023-12-10 22:07:58 +0300

    added js for the facts section

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const content1 = document.querySelector(".championship-content");
 const content2 = document.querySelector(".non-championship-content");
 const tab1 = document.querySelector("#tab1");
 const tab2 = document.querySelector("#tab2");
+const arrows = document.querySelectorAll(".arrow");
+const factsContent = document.querySelectorAll(".facts-content");
 
 tab1.addEventListener("click", () => {
   content1.style.display = "block";
@@ -17,4 +19,12 @@ tab2.addEventListener("click", () => {
   tab1.style.backgroundColor = "#aaaaaa";
   tab2.style.backgroundColor = "#ffff";
   tab1.style.borderTopRightRadius = "0";
+});
+
+arrows.forEach((arrow) => {
+  arrow.addEventListener("click", (e) => {
+    const contents = e.target.parentElement.nextElementSibling;
+    contents.style.display =
+      contents.style.display == "block" ? "none" : "block";
+  });
 });

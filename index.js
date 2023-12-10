692b401b321125384484ba825c76a1e6592b7960
shahana <rafiyashahana@gmail.com>
2023-12-10 17:40:26 +0300

    fix/championship table

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const content2 = document.querySelector(".non-championship-content");
 const tab1 = document.querySelector("#tab1");
 const tab2 = document.querySelector("#tab2");
 
-tab2.addEventListener("click", () => {
-  content1.style.display = "none";
-  content2.style.display = "block";
-});
-
 tab1.addEventListener("click", () => {
   content1.style.display = "block";
   content2.style.display = "none";
+  tab1.style.backgroundColor = "#ffff";
+  tab2.style.backgroundColor = "#aaaaaa";
+  tab2.style.borderTopLeftRadius = "0";
+});
+
+tab2.addEventListener("click", () => {
+  content1.style.display = "none";
+  content2.style.display = "block";
+  tab1.style.backgroundColor = "#aaaaaa";
+  tab2.style.backgroundColor = "#ffff";
+  tab1.style.borderTopRightRadius = "0";
 });
